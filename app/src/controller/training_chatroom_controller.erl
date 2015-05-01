e096a821143716ced6407265cfc27ff3f627ecc1
bryan hunt <github@binarytemple.co.uk>
2015-05-01 09:26:13 +0100

    Websockets chat example

--- app/src/controller/training_chatroom_controller.erl
+++ app/src/controller/training_chatroom_controller.erl
@@ -1,6 +1,10 @@
 -module(training_chatroom_controller, [Req]).
 -compile(export_all).
 
+console('GET', []) ->
+    lager:log(info, self(), "~p console", [ training_chatroom_controller ]),
+    {ok, [{user,uuid:to_string( uuid:uuid1() ) }]}.
+
 list('GET', []) ->
     lager:log(info, self(), "~p list", [ training_chatroom_controller ]),
     Chats = boss_db:find(chat, []),
